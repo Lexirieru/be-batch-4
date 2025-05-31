f362d5bdac0130235bff0e419fdcf2a7eb69a035
Lexirieru <axelmatsama@gmail.com>
2025-05-31 11:53:46 +0700

    Add indexer

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,12 +1,14 @@
-import { onchainTable } from "ponder";
+export const token = onchainTable("token", (t) => ({
+  id: t.text().primaryKey(),
+  address: t.text().notNull(),
+  name: t.text().notNull(),
+  symbol: t.text().notNull(),
+  decimals: t.integer().notNull(),
+}));
 
-// Data requirement:
-// Tx Hash
-// From (address)
-// To (address)
-// Amount
-// Timestamp
-// Block number
+export const tokenRelations = relations(token, ({ many }) => ({
+  transfers: many(transfer),
+}));
 
 export const transfer = onchainTable("transfer", (t) => ({
   id: t.text().primaryKey(),
@@ -16,6 +18,11 @@ export const transfer = onchainTable("transfer", (t) => ({
   amount: t.bigint().notNull(),
   timestamp: t.bigint().notNull(),
   blocknumber: t.bigint().notNull(),
+  tokenId: t.text().notNull(),
+}));
+
+export const transferRelations = relations(transfer, ({ one }) => ({
+  token: one(token, { fields: [transfer.tokenId], references: [token.id] }),
 }));
 
 export const holder = onchainTable("holder", (t) => ({
